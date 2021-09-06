b48d89d7638ce970fe1236f8a2257bd6fca1e84b
Kenneth512 <77126114+kennyalvarez0331@users.noreply.github.com>
2021-09-06 11:25:51 -0400

    added grid

---
+++ front-end/src/components/Map/Map.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+
+const Map = () => {
+  return (
+    <div>
+      <h1>Map</h1>
+    </div>
+  );
+};
+
+export default Map;
