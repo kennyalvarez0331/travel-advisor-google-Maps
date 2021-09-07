ea54686016dfddae9e95c545d62fff63a0975bbd
Kenneth512 <77126114+kennyalvarez0331@users.noreply.github.com>
2021-09-06 21:55:18 -0400

    almost done

--- front-end/src/components/Map/Map.jsx
+++ front-end/src/components/Map/Map.jsx
@@ -1,15 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
-import Rating from '@material-ui/lab';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const Map = () => {
+const Map = ({
+  setCoordinates,
+  setBounds,
+  coordinates,
+  places,
+  setChildClick,
+}) => {
+  const classes = useStyles();
+  const isDesktop = useMediaQuery('(min-width:600px)');
+
   return (
-    <div>
-      <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      {/* Google maps */}
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBBERfO-8LowWFUFNOdRFXOTx_ibtdRkA8' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClick(child)}>
+        {/* displays data on map  */}
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}>
+            {/* if not Desktop only display location Icon else cards on map */}
+            {!isDesktop ? (
+              <LocationOnOutlinedIcon color='primary' fontSize='large' />
+            ) : (
+              // card displays
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant='subtitle2'
+                  gutterBottom>
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+                  }
+                  alt={place.name}
+                />
+                <Rating size='small' value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 };
--- front-end/src/components/PlaceDetails/PlaceDetails.jsx
+++ front-end/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,10 +1,118 @@
 import React from 'react';
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Chip,
+} from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
 
-const PlaceDetails = () => {
+import useStyles from './styles';
+
+const PlaceDetails = ({ place }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>PlaceDetails</h1>
-    </div>
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 350 }}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+        }
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant='h5'>
+          {place.name}
+        </Typography>
+
+        {/* Price */}
+        <Box display='flex' justifyContent='space-between'>
+          <Rating value={Number(place.rating)} readOnly />
+          <Typography variant='subtitle1' gutterBottom>
+            Out of {place.num_reviews} reviews
+          </Typography>
+        </Box>
+
+        {/* Price */}
+        <Box display='flex' justifyContent='space-between'>
+          <Typography variant='subtitle1'>Price</Typography>
+          <Typography variant='subtitle1' gutterBottom>
+            {place.price_level}
+          </Typography>
+        </Box>
+
+        {/* Raking */}
+        <Box display='flex' justifyContent='space-between'>
+          <Typography variant='subtitle1'>Ranking</Typography>
+          <Typography variant='subtitle1' gutterBottom>
+            {place.ranking}
+          </Typography>
+        </Box>
+
+        {/* awards */}
+        {place?.awards?.map((award) => (
+          <Box my={1} display='flex' justifyContent='space-between'>
+            <img src={award.images.small} alt={award.display_name} />
+            <Typography variant='subtitle2' color='textSecondary'>
+              {award.display_name}
+            </Typography>
+          </Box>
+        ))}
+
+        {/* cuisines*/}
+        {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size='small' label={name} className={classes.chip} />
+        ))}
+
+        {/* address */}
+        {place?.address && (
+          <Typography
+            gutterBottom
+            variant='subtitle2'
+            color='textSecondary'
+            className={classes.subtitle}>
+            <LocationOnIcon /> {place.address}
+          </Typography>
+        )}
+
+        {/* phone */}
+        {place?.phone && (
+          <Typography
+            gutterBottom
+            variant='subtitle2'
+            color='textSecondary'
+            className={classes.subtitle}>
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
+
+        {/* trip advisor */}
+        <CardActions>
+          <Button
+            size='small'
+            color='primary'
+            onClick={() => window.open(place.web_url, '_black')}>
+            Trip Advisor
+          </Button>
+
+          {/* Website */}
+          <Button
+            size='small'
+            color='primary'
+            onClick={() => window.open(place.website, '_black')}>
+            Website
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
   );
 };
 
